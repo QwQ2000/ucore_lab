c1ebf18fd3eb4531bdf0f76322846419ca6053f7
QwQ2000 <396707050@qq.com>
2020-04-29 11:18:05 +0800

    lab1 no c99

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -291,10 +291,11 @@ read_eip(void) {
 void
 print_stackframe(void) {
     uint32_t ebp=read_ebp(),eip=read_eip();
-    for (int i=0;ebp && i<STACKFRAME_DEPTH;++i) {
+    int i=0,j=0;
+    for (;ebp && i<STACKFRAME_DEPTH;++i) {
         cprintf("ebp:0x%08x eip:0x%08x args:",ebp,eip);
         uint32_t* args=ebp+8;
-        for (int j=0;j<4;++j)
+        for (;j<4;++j)
             cprintf("0x%08x ",args[j]);
         cprintf("\n");
         print_debuginfo(eip-1);
